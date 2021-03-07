e9b0f8ee4a694884a25eddd7cd70c673262b661a
Martin Ender <m.buettner.d@gmail.com>
2021-03-07 21:22:39 +0100

    Add empty .NET Core project

---
+++ PolyformExplorer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PolyformExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
