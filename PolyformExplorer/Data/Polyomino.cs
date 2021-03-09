179ceed1bbb75ca2f9f2e57585f8e9e50aa39ce9
Martin Ender <m.buettner.d@gmail.com>
2021-03-10 00:46:39 +0100

    Add polyomino constructor

---
+++ PolyformExplorer/Data/Polyomino.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace PolyformExplorer.Data
+{
+    internal record Polyomino
+    {
+        public enum Symmetry
+        {
+            None,
+            MirrorOrthogonal,
+            MirrorDiagonal,
+            C2,
+            D2Orthogonal,
+            D2Diagonal,
+            C4,
+            D4,
+        };
+
+        private readonly ImmutableHashSet<IntVector2> cells;
+
+        public int Order => cells.Count;
+
+        public Polyomino()
+        {
+            IntVector2 origin = new IntVector2(0, 0);
+            cells = ImmutableHashSet.Create(origin);
+        }
+
+        public Polyomino(IEnumerable<IntVector2> cells)
+        {
+            IEnumerable<IntVector2> normalisedCells = Normalize(cells);
+            this.cells = ImmutableHashSet.CreateRange(normalisedCells);
+        }
+
+        private IEnumerable<IntVector2> Normalize(IEnumerable<IntVector2> cells)
+        {
+            int minX = cells.Min(cell => cell.X);
+            int minY = cells.Min(cell => cell.Y);
+
+            IntVector2 origin = new(minX, minY);
+
+            return cells.Select(cell => cell - origin);
+        }
+
+        public bool Contains(IntVector2 pos)
+            => cells.Contains(pos);
+    }
+}
---
+++ PolyformExplorerTests/Data/PolyominoTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace PolyformExplorer.Data.Tests
+{
+    internal class PolyominoTest
+    {   
+        [Test]
+        public void Default_constructor_returns_monomino()
+        {
+            Polyomino polyomino = new();
+            IntVector2 origin = new(0, 0);
+
+            Assert.That(polyomino.Order, Is.EqualTo(1));
+            Assert.That(polyomino.Contains(origin));
+        }
+
+        [Test]
+        public void Constructor_uses_custom_coordinates()
+        {
+            List<IntVector2> cells = new()
+            {
+                new(1, 0),
+                new(0, 1),
+                new(1, 1),
+            };
+
+            Polyomino polyomino = new(cells);
+
+            Assert.That(polyomino.Order, Is.EqualTo(3));
+            foreach (IntVector2 cell in cells)
+                Assert.That(polyomino.Contains(cell));
+        }
+
+        [Test]
+        public void Constructor_normalizes_position_of_cells()
+        {
+            List<IntVector2> cells = new()
+            {
+                new(3, -5),
+                new(2, -4),
+                new(3, -4),
+            };
+
+            List<IntVector2> expectedCells = new()
+            {
+                new(1, 0),
+                new(0, 1),
+                new(1, 1),
+            };
+
+            Polyomino polyomino = new(cells);
+
+            Assert.That(polyomino.Order, Is.EqualTo(3));
+            foreach (IntVector2 cell in expectedCells)
+                Assert.That(polyomino.Contains(cell));
+        }
+    }
+}
